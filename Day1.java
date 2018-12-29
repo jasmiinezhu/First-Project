35f7441f99b5500682cb0754ff66fa3387349ca7
tiger74s <zhujianjun@hotmail.com>
2018-12-29 11:58:18 -0800

    Update Day1.java
    Add Jeff Zhu first commit

--- Day1.java
+++ Day1.java
@@ -4,6 +4,7 @@ public class Day1 {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Accepted the invite to be a contributor"); 
     }
 
 }
